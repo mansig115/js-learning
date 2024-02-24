583807841b82a1e92afb62740b3c533269c4ce47
mansig115 <161108975+mansig115@users.noreply.github.com>
2024-02-24 08:02:32 +0000

    prettier

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13468099
+let accountEmail= "manusharma@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2
+
+accountEmail = "hgg@hc.com"
+accountPassword = "097653"
+accountCity = "Bangalore"
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") // we are using nodejs, node browser
+
+console.log(3+8)
+//code readability should be high
+console.log("Mansi")
