f4037ad4a1c14711d94fe3f7bef9105311b0a682
mansig115 <161108975+mansig115@users.noreply.github.com>
2024-03-02 07:52:28 +0000

    Num_Maths

---
+++ 1_basics/4_Num_Maths.js
@@ -0,0 +1,46 @@
+// const score = 5000
+// //console.log(score);
+
+// const balance = new Number(1000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 23.8966
+// const otherNumber = 1423.8966
+
+// console.log(otherNumber.toPrecision(5));
+
+// const hundreds =  1000000
+// console.log(hundreds.toLocaleString('en-IN'));   //this focus on the indian nuumber system like where to put commas
+
+
+//+++++++++++++++++++++++++++Maths++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //absolute means sign change like only change -4 to 4 thats all do not change + to -
+// console.log(Math.round(90.87));
+// console.log(Math.ceil(7.2));
+// console.log(Math.floor(7.5));
+// console.log(Math.min(3, 8,5,4));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random()); // random will contain only values between 0 & 1(0.1, 0.2)
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max -  min + 1))+1);
+
+
+
+
+
+
+
+
+
