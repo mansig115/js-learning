c209180b2de99ca927a50c757c1d706e833dbe1d
mansig115 <161108975+mansig115@users.noreply.github.com>
2024-03-09 15:14:34 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,32 @@
+// console.log("M");
+// console.log("A");
+// console.log("N");
+// console.log("S");
+// console.log("I");
+
+function  sayMyName(){
+    console.log("M");//function definition
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("I");
+
+}
+
+// sayMyName() this wont return anything its just a reference
+
+//Add two numbers function
+
+function addTwoNumbers(num1, num2){ //as the num1 
+    console.log(num1+num2);
+}
+
+// addTwoNumbers(5,6)
+// addTwoNumbers(3, "a") // prints -3a
+addTwoNumbers(3, null) // prints only 3
+
+ const result =  addTwoNumbers(3,5)
+ console.log("Result", result); //op-Result undefined
+
+
+
